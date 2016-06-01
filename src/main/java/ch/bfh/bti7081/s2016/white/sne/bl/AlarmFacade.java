2cddd266b510592710bfc4cc1237349797980846
Stefan Thöni <stefan.thoeni@hp.com>
2016-06-01 19:55:20 +0200

    * adding alarming facade and dao

---
+++ src/main/java/ch/bfh/bti7081/s2016/white/sne/bl/AlarmFacade.java
@@ -0,0 +1,10 @@
+package ch.bfh.bti7081.s2016.white.sne.bl;
+
+import java.util.List;
+
+import ch.bfh.bti7081.s2016.white.sne.data.Alarm;
+import ch.bfh.bti7081.s2016.white.sne.data.User;
+
+public interface AlarmFacade {
+	public List<Alarm> getAlarms(User user);
+}

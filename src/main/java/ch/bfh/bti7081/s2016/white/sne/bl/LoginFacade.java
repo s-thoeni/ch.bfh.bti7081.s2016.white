5560223b9fcc758f55d9efba60f072a2358cedc5
ellr <ellr@users.noreply.github.com>
2016-06-15 18:01:26 +0200

    * correct typos im comments

--- src/main/java/ch/bfh/bti7081/s2016/white/sne/bl/LoginFacade.java
+++ src/main/java/ch/bfh/bti7081/s2016/white/sne/bl/LoginFacade.java
@@ -12,20 +12,24 @@ import ch.bfh.bti7081.s2016.white.sne.data.exceptions.SneException;
  */
 public interface LoginFacade {
 	/**
-	 * @param username the username as string for which the password should be checked
-	 * @param Password the password as Strint to be checked
+	 * Checks the password for a given user name. Password must be specified as SHA256 Hash. If the passwords
+	 * match, a user object with provided user / password combination is returned.
+	 * @param username - the username as string for which the password should be checked
+	 * @param password - the password-hash (sha256) as String to be checked
 	 * @return The User object which matches with the entered one, of passwords match
 	 * @throws SneException
 	 */
 	public User checkPassword(String username, String Password) throws SneException;
+	
 	/**
-	 * 
+	 * Returns all users from the database
 	 * @return an Arraylist of all Users in the DB
 	 * @throws SneException
 	 */
 	public ArrayList<User> getUsers() throws SneException;
+	
 	/**
-	 * 
+	 * Returns the user object for a provided user name
 	 * @param username the username as string
 	 * @return a User object which's username matches with the given parameter
 	 * @throws SneException

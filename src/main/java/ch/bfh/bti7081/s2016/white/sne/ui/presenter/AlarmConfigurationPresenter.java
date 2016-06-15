3bf2602d39083291535c4201cc8a89316c596254
mcdizzu <matthias.sidler@iet-gibb.ch>
2016-06-15 15:41:34 +0200

    * fix bug

--- src/main/java/ch/bfh/bti7081/s2016/white/sne/ui/presenter/AlarmConfigurationPresenter.java
+++ src/main/java/ch/bfh/bti7081/s2016/white/sne/ui/presenter/AlarmConfigurationPresenter.java
@@ -26,28 +26,27 @@ import ch.bfh.bti7081.s2016.white.sne.ui.view.components.AlarmSet;
 import ch.bfh.bti7081.s2016.white.sne.ui.view.components.AlarmSetImpl;
 
 public class AlarmConfigurationPresenter implements AlarmConfigurationView.AlarmConfigurationViewListener {
-	
+
 	/**
 	 * Logger for this class
 	 */
 	private static final Logger logger = LogManager.getLogger(AlarmConfigurationPresenter.class);
-	
+
 	private AlarmConfigurationProvider model;
 	private AlarmConfigurationViewImpl view;
 
-	public AlarmConfigurationPresenter(AlarmConfigurationProvider model, AlarmConfigurationViewImpl view)
-			throws SneException {
+	public AlarmConfigurationPresenter(AlarmConfigurationProvider model, AlarmConfigurationViewImpl view) throws SneException {
 		this.model = model;
 		this.view = view;
-		
+
 		int i = 0;
 
-		for (Alarm al :  model.getAlarms()) {			
+		for (Alarm al : model.getAlarms()) {
 			AlarmSetImpl configSet = new AlarmSetImpl(al);
-			
+
 			configSet.setId(String.valueOf(i));
 			((AlarmSet) configSet).addListener(id -> deleteClick(id));
-			
+
 			view.addAlarmSet(configSet);
 			i++;
 		}
@@ -58,8 +57,8 @@ public class AlarmConfigurationPresenter implements AlarmConfigurationView.Alarm
 	@Override
 	public void addClick() {
 		logger.debug("->");
-		
-		int i = view.getAlarmSets().size() -1;
+
+		int i = view.getAlarmSets().size() - 1;
 		AlarmSetImpl alarmSet = new AlarmSetImpl();
 		alarmSet.setId(String.valueOf(i));
 		view.addAlarmSet(alarmSet);
@@ -70,7 +69,7 @@ public class AlarmConfigurationPresenter implements AlarmConfigurationView.Alarm
 	@Override
 	public void saveClick() throws SneException {
 		logger.debug("->");
-		
+
 		// DUMMY DATA
 		// FIXME: get user from elsewhere... where is it stored?
 		User user = new User("lucas.wirtz");
@@ -78,27 +77,23 @@ public class AlarmConfigurationPresenter implements AlarmConfigurationView.Alarm
 
 		for (AlarmSetImpl conf : this.view.getAlarmSets()) {
 			ReportTimeframe timeframe = conf.getReportTimeframe();
-			ReportType type = conf.getReportType();			
+			ReportType type = conf.getReportType();
 			ReportConfig repConf = new ReportConfig(type, timeframe);
-			
+
 			Operator comp = conf.getComparator();
 			int error = conf.getErrorValue();
 			int warn = conf.getWarnValue();
-			
+
 			Alarm newAlarm = new Alarm(repConf, error, warn, comp);
 			configuration.add(newAlarm);
-			
+
 		}
 		this.model.setAlarms(configuration, user);
 		MyUI ui = (MyUI) UI.getCurrent();
-		if(ui instanceof MyUI){
-			DashboardPresenter db = ui.getDashboard();
-			((SneMenuViewImpl) db.getView()).updateAlarms(this.model.getAlarms());
-			ui.getNavigationManager().navigateTo(db.getView());
-		}else{
-			Notification.show("We could not navigate corretly, please refresh the page manually");
-		}
-		
+		DashboardPresenter db = ui.getDashboard();
+		((SneMenuViewImpl) db.getView()).updateAlarms(this.model.getAlarms());
+		ui.getNavigationManager().navigateTo(db.getView());
+
 		logger.debug("<-");
 	}
 
@@ -119,15 +114,15 @@ public class AlarmConfigurationPresenter implements AlarmConfigurationView.Alarm
 	@Override
 	public void deleteClick(String id) {
 		logger.debug("->");
-		
+
 		List<AlarmSetImpl> alarmSets = view.getAlarmSets();
-		
-		for(int i =0; i<alarmSets.size();i++){
-			if(alarmSets.get(i).getId().equals(id)){
+
+		for (int i = 0; i < alarmSets.size(); i++) {
+			if (alarmSets.get(i).getId().equals(id)) {
 				view.deleteAlarmSet(alarmSets.get(i));
 			}
 		}
-		for(int i =0; i<alarmSets.size();i++){
+		for (int i = 0; i < alarmSets.size(); i++) {
 			alarmSets.get(i).setId(String.valueOf(i));
 		}
 		logger.debug("<-");
